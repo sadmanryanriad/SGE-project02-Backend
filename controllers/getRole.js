32edd38f3a8a8219eae0a3fac01d82745bd1b48f
sadmanryanriad <sadmanryanriad@gmail.com>
2024-08-06 17:27:01 +0600

    branch included in response

--- controllers/getRole.js
+++ controllers/getRole.js
@@ -20,7 +20,7 @@ const getRole = async (req, res) => {
     }
 
     // Respond with the user's role
-    res.status(200).json({ role: user.role });
+    res.status(200).json({ role: user.role, branch: user.branch });
   } catch (error) {
     console.error("Error fetching user role:", error);
     res.status(500).json({ message: "There was an internal error" });
