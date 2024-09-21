bcba380a47c9092de695fcd5128e2cec8018ff27
sadmanryanrad <sadmanryanriad@gmail.com>
2024-09-21 16:47:13 +0600

    updated URI

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.yv9dii9.mongodb.net/${process.env.DB}?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `${process.env.URI}`;
 const employeeRoute = require("./routes/employee");
 const signUp = require("./controllers/signUp");
 const login = require("./controllers/login");
